65934a20f124e1051628eec7ed4e060586e79be6
Anita Chen <b02902044@ntu.edu.tw>
2017-03-23 00:44:31 +0800

    modify python3.5_hw1.best.py to amount.py version

--- hw1/python3.5_hw1_best.py
+++ hw1/python3.5_hw1_best.py
@@ -6,11 +6,10 @@ data = np.genfromtxt(sys.argv[1], delimiter = ",")
 data = data[np.arange(10, 18 * 20 * 12, 18)]
 data = data[:, 3:]
 data = data.reshape(12, 480)
-data = data[2:6, :]
 
-x_data = np.empty((0, 9), float)
-y_data = np.empty((0, 1), float)
-for i in range(len(data)):
+src_data = np.empty((0, 9), float)
+res_data = np.empty((0, 1), float)
+for i in range(12):
     miss = []
     for j in range(data[i].size):
         if (data[i][j] == -1):
@@ -24,46 +23,51 @@ for i in range(len(data)):
         end = miss[j][1]
         data[i][begin : end] = np.interp(list(range(begin, end)), [begin - 1, end], [data[i][begin - 1], data[i][end]])
     for j in range(471):
-        x_data = np.append(x_data, [data[i][j:j + 9]], axis = 0)
-        y_data = np.append(y_data, [[data[i][j + 9]]], axis = 0)
-# print (x_data.shape, y_data.shape)
+        src_data = np.append(src_data, [data[i][j:j + 9]], axis = 0)
+        res_data = np.append(res_data, [[data[i][j + 9]]], axis = 0)
 
-# y_data = b + w * x_data 
-b = 0.0 # initial b
-w = np.zeros((9, 1)) # initial w
-lr = 0.5 # learning rate
-iteration = 30000
+v_src_data = src_data[5181:]
+v_res_data = res_data[5181:]
 
-b_lr = 0.0
-w_lr = np.zeros((9, 1))
-lamda = 10000
+for n in range(4, 5):
+    for start in range(2, 3):
+        if start + n > 11:
+            break
+        x_data = src_data[start * 471 : (start + n) * 471]
+        y_data = res_data[start * 471 : (start + n) * 471]
 
-# Iterations
-for k in range(iteration):
-    # if k % 1000 == 999:
-    #     print ("round", k + 1)
-    
-    b_grad = 0.0
-    w_grad = np.zeros((9, 1))
+        # y_data = b + w * x_data 
+        b = 0.0 # initial b
+        w = np.zeros((9, 1)) # initial w
+        lr = 0.5 # learning rate
+        iteration = 10000
 
-    tmp = 2.0 * (y_data - b - np.dot(x_data, w))
-    b_grad = b_grad - np.sum(tmp)
-    w_grad = w_grad - np.dot(tmp.T, x_data).T# + 2 * lamda * w
-    
-    b_lr = b_lr + b_grad ** 2
-    w_lr = w_lr + w_grad ** 2
-    
-    # Update parameters.
-    b = b - lr/np.sqrt(b_lr) * b_grad
-    w = w - lr/np.sqrt(w_lr) * w_grad
+        b_lr = 0.0
+        w_lr = np.zeros((9, 1))
+        lamda = 10000
 
+        # Iterations
+        for k in range(iteration):
+            b_grad = 0.0
+            w_grad = np.zeros((9, 1))
 
-test = np.genfromtxt(sys.argv[2], delimiter = ",")
-test = test[np.arange(9, 18 * 20 * 12, 18)]
-test = test[:, 2:]
-output = np.array(b + test.dot(w))
+            tmp = 2.0 * (y_data - b - np.dot(x_data, w))
+            b_grad = b_grad - np.sum(tmp)
+            w_grad = w_grad - np.dot(tmp.T, x_data).T# + 2 * lamda * w
+            
+            b_lr = b_lr + b_grad ** 2
+            w_lr = w_lr + w_grad ** 2
+            
+            # Update parameters.
+            b = b - lr/np.sqrt(b_lr) * b_grad
+            w = w - lr/np.sqrt(w_lr) * w_grad
 
-f = open(sys.argv[3], 'w')
-f.write('id,value\n')
-for k in range(240):
-	f.write("id_{},{}\n".format(k, output[k, 0]))
+        test = np.genfromtxt(sys.argv[2], delimiter = ",")
+        test = test[np.arange(9, 18 * 20 * 12, 18)]
+        test = test[:, 2:]
+        output = np.array(b + test.dot(w))
+
+        f = open(sys.argv[3], 'w')
+        f.write('id,value\n')
+        for k in range(240):
+            f.write("id_{},{}\n".format(k, output[k, 0]))
